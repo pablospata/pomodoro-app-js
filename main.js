9af7ad65955262e61643c9a1ece6096ec07ecce7
pablospata <pablospata@hotmail.com>
2023-08-02 20:48:36 -0300

    Corregir audio

--- main.js
+++ main.js
@@ -11,13 +11,26 @@ const audio = document.getElementById('audio');
 const iniciar = document.getElementById('iniciar');
 /*
 audio.addEventListener('ended', function () {
-    this.currentTime = 2;
-    
-    this.play();
-    this.pause();
+    this.currentTime = 30;    
+    this.play();    
+
 }, false);
 */
+
 iniciar.addEventListener('click', function () {
     console.log(audio)
+
+    // audio.currentTime = 50;    
+
     audio.play();
+});
+
+const finDelAudio = 59;
+audio.addEventListener('timeupdate', () => {
+    console.log(audio.currentTime);
+
+    if (audio.currentTime >= finDelAudio) {
+        audio.currentTime = 0.8; 
+        audio.play(); 
+    }
 });
