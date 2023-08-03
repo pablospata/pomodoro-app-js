b5ea3da602f8dca26d63458cf59544364fecfc26
pablospata <pablospata@hotmail.com>
2023-08-03 16:46:16 -0300

    Agregar timeWorker

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const stopwatch = document.getElementById('stopwatch');
 const ding = document.getElementById('ding');
+const dong = document.getElementById('dong');
 const ring = document.getElementById('ring');
 const closes = document.getElementById('closes');
 const iniciarBoton = document.querySelector('.start');
 const pararBoton = document.querySelector('.stop');
+const elapsedProgress = document.querySelector('.elapsed');
+const breakProgress = document.querySelector('.break');
 
 iniciarBoton.addEventListener('click', function () {
     ding.play();
@@ -17,12 +20,55 @@ pararBoton.addEventListener('click', function () {
     closes.play();
 
     stopwatch.currentTime = 0;
+
 });
 
-stopwatch.addEventListener('timeupdate', () => {    
-    
-    if (stopwatch.currentTime >= 25*60) {
+function estiloLongBreak() {
+    elapsedProgress.style.width = 'calc(' + 25 + ' * 100% / ' + 40 + ')';
+    breakProgress.style.width = 'calc(' + 15 + ' * 100% / ' + 40 + ')';
+}
+
+
+document.querySelector('.start').addEventListener('click', () => {
+    worker.postMessage('start');
+});
+
+document.querySelector('.stop').addEventListener('click', () => {
+    worker.postMessage('stop');
+    document.querySelector('.progress span').textContent = '25:00';
+    document.querySelector('.elapsed').value = 0;
+});
+
+const worker = new Worker('timerWorker.js');
+
+worker.addEventListener('message', (event) => {
+    if (event.data.type === 'update') {
+        updateTimer(event.data.timeLeft, event.data.breakTimeLeft);
+    } else if (event.data.type === 'break') {
         ring.play();
         stopwatch.pause();
+        onBreak = true;
+    } else if (event.data.type === 'done') {
+        dong.play();
+        document.querySelector('.elapsed').value = 0;
+        document.querySelector('.break').value = 0;
+        document.querySelector('.progress span').textContent = '25:00';
     }
 });
+
+let onBreak = false;
+function updateTimer(timeLeft, breakTimeLeft) {
+    if (!onBreak) {
+        document.querySelector('.elapsed').value = 1500 - timeLeft;
+        document.querySelector('.progress span').textContent = formatTime(timeLeft);
+    } else {
+        document.querySelector('.break').value = 300 - breakTimeLeft;
+        document.querySelector('.progress span').textContent = formatTime(breakTimeLeft);
+    }
+}
+
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${String(remainingSeconds).padStart(2, '0')}`;
+}
---
+++ timerWorker.js
@@ -0,0 +1,29 @@
+let timer;
+let timeLeft = 10; // 25 minutos en segundos
+let breakTimeLeft = 7; // 5 minutos en segundos
+let onBreak = false;
+
+self.addEventListener('message', (event) => {
+    if (event.data === 'start') {
+        clearInterval(timer); // Por si acaso hay uno ya funcionando
+        timer = setInterval(() => {
+            if (!onBreak) {
+                timeLeft--;
+                if (timeLeft < 0) {
+                    onBreak = true;
+                    self.postMessage({ type: 'break' }); 
+                }
+            } else {
+                breakTimeLeft--;
+                if (breakTimeLeft < 0) {
+                    clearInterval(timer);
+                    self.postMessage({ type: 'done' });
+                    return;
+                }
+            }
+            self.postMessage({ type: 'update', timeLeft: Math.max(0, timeLeft), breakTimeLeft: Math.max(0, breakTimeLeft) });
+        }, 1000);
+    } else if (event.data === 'stop') {
+        clearInterval(timer);
+    }
+});
