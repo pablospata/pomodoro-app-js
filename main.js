b95a5e3fb6fb5dafdfe5606ad61da4b1fba290f3
pablospata <pablospata@hotmail.com>
2023-08-03 01:08:14 -0300

    Agregar estilos

--- main.js
+++ main.js
@@ -1,36 +1,17 @@
-/*let audio = document.getElementById('audio');
-
-console.log(audio)
-
-
-audio.play();
-*/
-
 const audio = document.getElementById('audio');
-
 const iniciar = document.getElementById('iniciar');
-/*
-audio.addEventListener('ended', function () {
-    this.currentTime = 30;    
-    this.play();    
-
-}, false);
-*/
 
 iniciar.addEventListener('click', function () {
-    console.log(audio)
-
-    // audio.currentTime = 50;    
-
+    audio.currentTime = inicioDelAudio;    
     audio.play();
 });
 
-const finDelAudio = 59;
-audio.addEventListener('timeupdate', () => {
-    console.log(audio.currentTime);
-
+const inicioDelAudio = 0
+const finDelAudio = 60;
+audio.addEventListener('timeupdate', () => {    
     if (audio.currentTime >= finDelAudio) {
-        audio.currentTime = 0.8; 
+        audio.currentTime = inicioDelAudio;         
         audio.play(); 
     }
 });
+
