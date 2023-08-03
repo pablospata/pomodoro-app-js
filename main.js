73fc69a9fc7dd1cf66e6d19a5d78582c7bd4a2ec
pablospata <pablospata@hotmail.com>
2023-08-03 09:54:47 -0300

    Corregir Audio

--- main.js
+++ main.js
@@ -1,63 +1,12 @@
-/*const audio = document.getElementById('audio');
-const iniciar = document.querySelector('.start');
-
-iniciar.addEventListener('click', function () {
-    audio.currentTime = inicioDelAudio;    
-    audio.play();
-});
-
-const inicioDelAudio = 0.8
-const finDelAudio = 59;
-audio.addEventListener('timeupdate', () => {    
-    if (audio.currentTime >= finDelAudio) {
-        audio.currentTime = inicioDelAudio;         
-        audio.play(); 
-    }
-});*/
-
 const audio = document.getElementById('audio');
 const iniciarBoton = document.querySelector('.start');
 const pararBoton = document.querySelector('.stop');
-const inicioDelAudio = 0;
-// const finDelAudio = 0.910;
-const finDelAudio = 0.899;
-let isPlaying = false;
 
-iniciarBoton.addEventListener('click', function () {
-    audio.currentTime = inicioDelAudio;
-    audio.play();
-    isPlaying = true;
-    updateAudioTime();
+iniciarBoton.addEventListener('click', function () {    
+    audio.play();    
 });
 
 pararBoton.addEventListener('click', function () {
-    audio.currentTime = inicioDelAudio;
     audio.pause();
-    isPlaying = false;
+    audio.currentTime = 0;    
 });
-
-function updateAudioTime() {
-    if (isPlaying) {
-        if (audio.currentTime >= finDelAudio) {
-            audio.currentTime = inicioDelAudio;
-            audio.play();
-        }
-        // console.log(audio.currentTime);
-        requestAnimationFrame(updateAudioTime);
-    }
-}
-
-audio.addEventListener('play', () => {
-    isPlaying = true;
-    updateAudioTime();
-});
-
-audio.addEventListener('pause', () => {
-    isPlaying = false;
-});
-
-audio.addEventListener('ended', () => {
-    isPlaying = false;
-});
-
-
