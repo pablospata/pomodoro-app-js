26b5301671c83fb5ccca2a3ea0279cd33f4a517d
pablospata <pablospata@hotmail.com>
2023-08-03 14:06:35 -0300

    Agregar audios interfaz

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
-const audio = document.getElementById('audio');
+const stopwatch = document.getElementById('stopwatch');
+const ding = document.getElementById('ding');
+const ring = document.getElementById('ring');
+const closes = document.getElementById('closes');
 const iniciarBoton = document.querySelector('.start');
 const pararBoton = document.querySelector('.stop');
 
-iniciarBoton.addEventListener('click', function () {    
-    audio.play();    
+iniciarBoton.addEventListener('click', function () {
+    ding.play();
+    setTimeout(() => {
+        stopwatch.play();
+    }, 600);
 });
 
 pararBoton.addEventListener('click', function () {
-    audio.pause();
-    audio.currentTime = 0;    
+    stopwatch.pause();
+    closes.play();
+
+    stopwatch.currentTime = 0;
+});
+
+stopwatch.addEventListener('timeupdate', () => {    
+    
+    if (stopwatch.currentTime >= 25*60) {
+        ring.play();
+        stopwatch.pause();
+    }
 });
